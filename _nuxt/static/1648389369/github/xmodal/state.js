a36719e7b60381df611012618d3091db9a1ae552
mediv0 <anonymousaxis@gmail.com>
2022-03-27 18:26:15 +0430

    7ce1c54

---
+++ _nuxt/static/1648389369/github/xmodal/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1648389369",layout:"non-header",error:null,state:{isNavOpen:a,isNavDisabled:a,currentProjectIdInTrackPage:-1,isProjectTokenHasError:a,routerLoadingEnable:b,previousRoute:"\u002F"},serverRendered:b,routePath:"\u002Fgithub\u002Fxmodal",config:{}}}(false,true));
